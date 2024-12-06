15a05aa1e5b6f13182c5fdd6255541f0ecce9b60
AsherValentini <ashermvalentini@gmail.com>
2024-12-06 17:47:47 +0100

    adding tests for api_model class weatherAPI

---
+++ tests/test_api_model_object.py
@@ -0,0 +1,16 @@
+from models.api_model import WeatherAPI
+
+
+def test_weather_api_valid_city():
+    api = WeatherAPI("de4dfcfd5ab05531b01c5bcd9c923e3c")
+    status_code, result = api.fetch_weather("London")
+    assert "temperature" in result
+    assert "description" in result
+    assert result["city"] == "London"
+
+
+def test_weather_api_invalid_city():
+    api = WeatherAPI("some incorrect api key")
+    result = api.fetch_weather("London")
+    assert "error" in result
+    assert result["error"] == 404
