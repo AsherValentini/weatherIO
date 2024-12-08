9f4c01652296274180a316cb8d9ce44afe365e9f
AsherValentini <ashermvalentini@gmail.com>
2024-12-08 19:11:39 +0100

    test scripts for the api model are updated and show module functionality

--- tests/test_api_model.py
+++ tests/test_api_model.py
@@ -7,15 +7,17 @@ def api():
     return WeatherAPI("de4dfcfd5ab05531b01c5bcd9c923e3c")
 
 
-def test_fetch_weather_valid_city(api):
-    result = api.fetch_weather("London")
+@pytest.mark.asyncio
+async def test_fetch_weather_valid_city(api):
+    result = await api.fetch_weather("London")
     assert "city" in result
     assert "temperature" in result
     assert "description" in result
     assert result["city"] == "London"
 
 
-def test_fetch_weather_invalid_city(api):
-    result = api.fetch_weather("invalid city")
+@pytest.mark.asyncio
+async def test_fetch_weather_invalid_city(api):
+    result = await api.fetch_weather("invalid city")
     assert "error" in result
     assert isinstance(result["error"], (int, str))
